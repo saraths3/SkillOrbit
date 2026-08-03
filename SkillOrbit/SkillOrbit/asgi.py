fe55a47f6512c9971c684365236be0820a43b9d4
sarath-s <sarath.work@zohomail.in>
2026-08-03 19:20:18 +0530

    Fix ASGI initialization order in asgi.py

--- SkillOrbit/SkillOrbit/asgi.py
+++ SkillOrbit/SkillOrbit/asgi.py
@@ -1,21 +1,15 @@
-
 import os
-
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE",
-    "SkillOrbit.settings"
-)
-
-
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 
-from chat.routing import websocket_urlpatterns
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SkillOrbit.settings")
 
-
+# Initialize Django ASGI application FIRST so AppRegistry is loaded!
 django_asgi_app = get_asgi_application()
 
+# Import Channels and Routing AFTER Django is initialized
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
